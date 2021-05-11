75e35888db2d9aab18a5beaef5f25e6f959b15f5
Evan <78163054+Evd-V@users.noreply.github.com>
2021-05-11 10:41:12 +0200

    Added documentation

--- lyapunov.py
+++ lyapunov.py
@@ -1,31 +1,39 @@
 import numpy as np
 
 def norm_vect(vector):
+    """ Calculate the norm of a vector. """
     values = [i*i for i in vector]
     return sum(values)[0]
 
 def inner_vect(vect1, vect2):
+    """ Calculate the inner product of two vectors. """
     values = [vect1[i] * vect2[i] for i in range(len(vect1))]
     return sum(values)[0]
-    
+
 def proj_vect(vect1, vect2):
+    """ Calculate the projection of vector v onto vector u. """
     return (inner_vect(vect1, vect2) / norm_vect(vect1)) * vect1
     
 def Gram_Schmidt_rev(vectors):
-    """ 
+    """ Function that uses the Gram-Schmidt process to orthogonalize a set of n-dimensional 
+        vectors. The normalization of the vectors is not included.
+        
+        Input: vectors = list containing the vectors; a valid input is for example:
+                         [v1, v2] where v1 = [[x1], [y1]] and v2 = [[x2], [y2]];
+                         
+        Returns: basis = list containing the orthogonalised set of vectors in the same format 
+                         as the input 'vectors'.
     """
     basis = [vectors[0]]
     for v in range(1, len(vectors)):
         for j in range(v):
-            value = proj_vect(vectors[j], vectors[v])
-            new_vect = vectors[v] - value
-            
+            new_vect = vectors[v] - proj_vect(vectors[j], vectors[v])
         basis.append(new_vect)
             
     return basis
 
 def Lyapunov(N, basis, xvalues, k):
-    """ 
+    """ Function that calculates the Lyapunov exponents. Still work in progress.
     """
     
     dim = len(basis)
